7fcb43ed128519d588d3b81be136c3d6b1e9fda8
Ubuntu <ubuntu@concourse.pf0emgvb42cufewuz5nqf4i1yf.syx.internal.cloudapp.net>
2022-04-27 16:59:19 +0000

    edit

--- web-task.sh
+++ web-task.sh
@@ -2,17 +2,23 @@
   
 set -e
 
-#cd /var/www/html
-##sed -i 's/world/yeeun/' /var/www/html/index.html
-#sed -i 's/yeeun/world/' index.html
-#
+sudo apt-get install apache2 -y
+rm -rf /var/www/html/index.html
+sudo touch /var/www/html/index.html
+sudo echo "<h1>hello world</h1>" > /var/www/html/index.html
+sudo chmod 777 /var/www/html/index.html
 
-docker run -it --name web centos
-yum install net-tools -y
-ifconfig
-yum install httpd -y
-cd /var/www/html
-rm index.html
-echo "<h1>hello world<h2>"
-systemctl start httpd
-exit
+#cd /var/www/html
+#sed -i 's/world/yeeun/' /var/www/html/index.html
+#sed -i 's/yeeun/world/' index.html
+
+
+#docker run -it --name web centos
+#yum install net-tools -y
+#ifconfig
+#yum install httpd -y
+#cd /var/www/html
+#rm index.html
+#echo "<h1>hello world<h2>"
+#systemctl start httpd
+#exit
